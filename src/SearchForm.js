adee3f12df1ee0253d30b55896cf8c96787efc44
Nathan Rodman <nathanrodman@gmail.com>
2017-03-01 19:09:24 -0800

    initial commit

---
+++ src/SearchForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchForm extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        
+    }
+}
