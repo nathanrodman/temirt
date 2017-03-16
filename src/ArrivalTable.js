45f59de7a4646c29f376e674412ce13b62ee6c91
Nathan Rodman <nathanrodman@gmail.com>
2017-03-15 20:06:19 -0700

    Detour component added

--- src/ArrivalTable.js
+++ src/ArrivalTable.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 
 import Arrival from './Arrival';
+import Detour from './Detour';
 
 export default class ArrivalTable extends Component {
 
@@ -15,7 +16,9 @@ export default class ArrivalTable extends Component {
       <div>
         <h3>{arrivals.location && `${arrivals.location[0].id} - ${arrivals.location[0].desc}`}</h3>
         <ul>
-          {/* arrivals.detour && <Detour detourInfo={arrivals.detour} */}
+          { arrivals.detour && arrivals.detour.map((value, index) => {
+            return <Detour detourInfo={value} /> 
+    })}
           {arrivals.error ? <li>{arrivals.error.content}</li> :
             arrivals.arrival ? arrivals.arrival.map((value, index) => {
               return <Arrival arrivalData={value} />;
---
+++ src/Detour.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+export default class Detour extends Component {
+  /*
+  {
+      "route": [{
+        "route": 15,
+        "detour": true,
+        "type": "B",
+        "desc": "15-Belmont\/NW 23rd"
+      }],
+      "info_link_url": "",
+      "end": 1607015040000,
+      "id": "39017",
+      "begin": 1428832800000,
+      "desc": "Buses continue to use the Hawthorne Bridge due to weight restrictions on the Morrison Bridge."
+    }
+    */
+  
+  render() {
+    return (<div>
+      <h3>{this.props.detourInfo.desc}</h3>
+    </div>
+    )
+  }
+}
